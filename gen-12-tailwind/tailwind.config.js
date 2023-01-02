6eafa4587c7bbcc3b132c5c288f64d17c7194492
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-02 13:56:18 +0700

    config & custom

--- gen-12-tailwind/tailwind.config.js
+++ gen-12-tailwind/tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        utama: '#def03c'
+      },
+      spacing: {
+        '18': '4.5rem'
+      }
+    },
   },
   plugins: [],
 }
