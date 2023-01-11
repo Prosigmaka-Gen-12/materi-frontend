a4e0aceceaf248a04993d15e0bf83e5e01d6ab11
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-11 12:50:22 +0700

    import export

--- gen-12-react-proper/src/App.js
+++ gen-12-react-proper/src/App.js
@@ -1,6 +1,9 @@
+import ClassExample from "./ClassExample"
+
 function App () {
   return <>
-    <h1>Hello World</h1>
+    <h1>Hello World!</h1>
+    <ClassExample />
   </>
 }
 
---
+++ gen-12-react-proper/src/ClassExample.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ClassExample extends Component {
+	render () {
+		return <>
+			<h2>Aku Class Component</h2>
+		</>
+	}
+}
+
+export default ClassExample
