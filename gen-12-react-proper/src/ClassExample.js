1a6599ee35da6adae7d475961dba9e61ce335aec
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-11 14:17:29 +0700

    lifecycle

--- gen-12-react-proper/src/ClassExample.js
+++ gen-12-react-proper/src/ClassExample.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 
 class ClassExample extends Component {
+	constructor (props) {
+		super(props)
+		this.state = {
+			name: 'Abid',
+			age: 17
+		}
+	}
+
+	componentDidMount () {
+		console.log('ini did mount')
+	}
+
+	componentDidUpdate () {
+		console.log('ini did update')
+	}
+
 	render () {
+		console.log('ini render')
 		return <>
 			<h2>Aku Class Component</h2>
+			<ul>
+				<li>namaku: {this.state.name}</li>
+				<li>umurku: {this.state.age}</li>
+			</ul>
+			<button onClick={() => this.setState({ name: 'Bambang' }) }>
+				Ubah Nama
+			</button>
+			<button onClick={() => this.setState({ age: 31 }) }>
+				Ubah Umur
+			</button>
 		</>
 	}
 }
