105ad65fb8a59933082d426addd6ffacfbe28f84
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-12 15:06:07 +0700

    complex loop

--- gen-12-react-vite-useeffect/src/App.jsx
+++ gen-12-react-vite-useeffect/src/App.jsx
@@ -1,15 +1,32 @@
 function App () {
   const cars = [
-    'Gymkhana',
-    'Ford',
-    'BMW'
+    {
+      id: 1,
+      make: 'Subaru',
+      model: 'Impreza',
+      year: '1992'
+    },
+    {
+      id: 2,
+      make: 'Nissan',
+      model: 'R34 Skyline GT-R Spec V',
+      year: '2002'
+    },
+    {
+      id: 3,
+      make: 'Mazda',
+      model: 'RX 7',
+      year: '2002'
+    }
   ]
 
   return <>
     <h1>Hai Bambang</h1>
 
-    {cars.map((car) =>
-      <em>{car}</em>
+    {cars.map(car =>
+      <p key={car.id}>
+        {car.make} {car.model}
+      </p>
     )}
   </>
 }
