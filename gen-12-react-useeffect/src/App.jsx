6db8f393d3482280e6f0f729f2c559eed8507926
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-17 10:47:29 +0700

    use effect demo

--- gen-12-react-useeffect/src/App.jsx
+++ gen-12-react-useeffect/src/App.jsx
@@ -1,8 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-function App() {
+function App () {
+  const [isShow, setIsShow] = useState(true)
+
+  return <>
+    <h1>ini app</h1>
+    <button onClick={() => setIsShow(false)}>
+      sembunyikan children
+    </button>
+    <hr />
+    {isShow ? <Children /> : null}
+  </>
+}
+
+function Children() {
+  const [count, setCount] = useState(0)
+  const [down, setDown] = useState(100)
+
+  useEffect(() => {
+    console.log('Aku log setiap count berubah')
+  }, [count])
+
+  useEffect(() => {
+    console.log('Aku log setiap down berubah')
+
+    return () => {
+      console.log('aku jalan SEBELUM render berikutnya')
+    }
+  }, [down])
+
+  useEffect(() => {
+    console.log('Aku log di awal aja')
+
+    return () => {
+      console.log('aku log di paling akhir')
+    }
+  }, [])
+
   return <>
     <h2>yo</h2>
+    <p onClick={() => setCount(count + 1)}>{count}</p>
+    <p onClick={() => setDown(down - 1)}>{down}</p>
   </>
 }
 
