b14750bf6837315e4d459647ec863644ee5355cd
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-18 11:54:30 +0700

    using form tag

--- gen-12-react-form-handling/src/App.jsx
+++ gen-12-react-form-handling/src/App.jsx
@@ -19,40 +19,54 @@ function App () {
     // setFormInput({ ...formInput, [type]: value })
   }
 
+  const submitForm = (evt) => {
+    evt.preventDefault()
+    console.log(formInput)
+  }
+
   return <>
-    <label>
-      Email:
-      <input type="text" value={formInput.email} onChange={(evt) => handleInput('email', evt.target.value)} />
-    </label>
-    <br />
-    <label>
-      Tanggal Lahir:
-      <input type="date" value={formInput.birthDate} onChange={(evt) => handleInput('birthDate', evt.target.value)} />
-    </label>
-    <br />
-    <label>
-      Agama:
-      <select value={formInput.religion} onChange={(evt) => handleInput('religion', evt.target.value)}>
-        <option value=""></option>
-        <option value="islam">islam</option>
-        <option value="kristen protestan">kristen protestan</option>
-        <option value="katolik">katolik</option>
-        <option value="hindu">hindu</option>
-        <option value="budha">budha</option>
-      </select>
-    </label>
+    <form onSubmit={submitForm}>
+      <label>
+        Email:
+        <input required type="text" value={formInput.email} onChange={(evt) => handleInput('email', evt.target.value)} />
+      </label>
+      <br />
+      <label>
+        Tanggal Lahir:
+        <input type="date" value={formInput.birthDate} onChange={(evt) => handleInput('birthDate', evt.target.value)} />
+      </label>
+      <br />
+      <label>
+        Agama:
+        <select value={formInput.religion} onChange={(evt) => handleInput('religion', evt.target.value)}>
+          <option value=""></option>
+          <option value="islam">islam</option>
+          <option value="kristen protestan">kristen protestan</option>
+          <option value="katolik">katolik</option>
+          <option value="hindu">hindu</option>
+          <option value="budha">budha</option>
+        </select>
+      </label>
 
-    <br />
+      <br />
 
-    <p>Jenis Kelamin</p>
-    <label>
-      <input type="radio" value="l" checked={formInput.gender === 'l'} onChange={() => handleInput('gender', 'l')} />
-      Laki Laki
-    </label>
-    <label>
-      <input type="radio" value="p" checked={formInput.gender === 'p'} onChange={() => handleInput('gender', 'p')} />
-      Perempuan
-    </label>
+      <p>Jenis Kelamin</p>
+      <label>
+        <input type="radio" value="l" checked={formInput.gender === 'l'} onChange={() => handleInput('gender', 'l')} />
+        Laki Laki
+      </label>
+      <label>
+        <input type="radio" value="p" checked={formInput.gender === 'p'} onChange={() => handleInput('gender', 'p')} />
+        Perempuan
+      </label>
+
+      <br />
+
+      <button>
+        SUBMIT
+      </button>
+
+    </form>
   </>
 }
 
