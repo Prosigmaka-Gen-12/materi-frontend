cb10b8ef5a11b85060da39b1a30591d62609552e
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-20 10:16:04 +0700

    test api then catch

--- gen-12-react-api-call/src/App.jsx
+++ gen-12-react-api-call/src/App.jsx
@@ -3,8 +3,16 @@ import axios from 'axios'
 
 function App() {
 
+  const testApiThenCatch = () => {
+    axios.get('https://dummyjson.com/products/1')
+      .then(res => console.log(res))
+  }
+
   return <>
     <h1>weiii</h1>
+    <button onClick={testApiThenCatch}>
+      Test Api
+    </button>
   </>
 }
 
