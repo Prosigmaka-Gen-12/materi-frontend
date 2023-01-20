3291239ab271f3448b4644188ef72373b568ddd6
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-01-20 10:33:37 +0700

    async await finally

--- gen-12-react-api-call/src/App.jsx
+++ gen-12-react-api-call/src/App.jsx
@@ -2,16 +2,37 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
 
   const testApiThenCatch = () => {
-    axios.get('https://dummyjson.com/products/1')
+    setIsLoading(true)
+    axios.put('https://dummyjson.com/products/1')
       .then(res => console.log(res))
+      .catch(err => console.log(err))
+      .finally(() => setIsLoading(false))
   }
 
-  return <>
+  const testApiAsyncAwait = async () => {
+    try {
+      setIsLoading(true)
+      const res = await axios.put('https://dummyjson.com/products/1')
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  return isLoading
+  ? <h1>Loading...</h1>
+  : <>
     <h1>weiii</h1>
     <button onClick={testApiThenCatch}>
-      Test Api
+      Test Api Then Catch
+    </button>
+    <button onClick={testApiAsyncAwait}>
+      Test Api Async Await
     </button>
   </>
 }
