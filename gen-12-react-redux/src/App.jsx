6814ed5a2c397004ebad38425a3fbed323bdf6c3
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-02-01 10:51:50 +0700

    dispatch an action

--- gen-12-react-redux/src/App.jsx
+++ gen-12-react-redux/src/App.jsx
@@ -1,10 +1,21 @@
+import { useDispatch } from "react-redux"
+
 import Footer from "./Footer"
 import Header from "./Header"
 
 function App() {
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    dispatch({
+      type: 'user/change_name',
+      payload: 'Bambang'
+    })
+  }
+
   return <>
     <Header />
-    <h1>test</h1>
+    <h1 onClick={handleClick}>test</h1>
     <Footer />
   </>
 }
