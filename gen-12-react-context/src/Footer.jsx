edea8f0cf7693a41973c745ba52d0ee9694d25fc
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-02-01 10:38:13 +0700

    setup redux

--- gen-12-react-context/src/Footer.jsx
+++ gen-12-react-context/src/Footer.jsx
@@ -2,11 +2,12 @@ import { useContext } from 'react'
 import { UserContext } from './UserProvider'
 
 export default function Footer () {
-	const { age } = useContext(UserContext)
+	const { age, name } = useContext(UserContext)
 
 	return <>
 		<hr />
 		<h2>ini footer</h2>
 		<i>{age}</i>
+		<marquee>{name}</marquee>
 	</>
 }
---
+++ gen-12-react-redux/src/userReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+	name: 'Abid',
+	age: 17
+}
+
+function userReducer(state = initialState, action) {
+	switch (action.type) {
+		case 'user/change_name':
+			return { ...state, name: action.payload }
+		case 'user/change_age':
+			return { ...state, age: action.payload }
+	}
+}
