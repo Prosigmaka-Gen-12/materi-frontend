4c6d271aa185d27a56365359468d9cee3a82f07c
AbidKhairyAK <abid.khairy.ak@gmail.com>
2023-02-02 11:06:46 +0700

    change multi value

--- gen-12-react-redux-toolkit/src/Header.jsx
+++ gen-12-react-redux-toolkit/src/Header.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux"
-import { change_title } from "./articleSlice"
+import { change_body_and_author, change_title } from "./articleSlice"
 
 export default function Header () {
 	const dispatch = useDispatch()
@@ -10,11 +10,21 @@ export default function Header () {
 		dispatch(change_title('Bambang Melahirkan Anak Tetangga'))
 	}
 
+	const handleBody = () => {
+		dispatch(change_body_and_author({
+			body: 'Anim deserunt dolore sunt reprehenderit pariatur ea quis dolore consequat ea reprehenderit veniam id. Ipsum ullamco tempor cupidatat do ullamco amet sint tempor non commodo aliqua ex velit. Dolore est ipsum eu est minim aliqua id minim exercitation anim eu occaecat. Ut ad deserunt duis eiusmod magna laboris.',
+			author: 'Bambang Suyatno'
+		}))
+	}
+
 	return <>
 		<h2>Ini Header</h2>
 		<h3>{user.name}</h3>
 		<h3>{article.title}</h3>
+		<p>{article.body}</p>
+		<p>{article.author}</p>
 		<button onClick={handleClick}>change article title</button>
+		<button onClick={handleBody}>change article body & author</button>
 		<hr />
 	</>
 }
